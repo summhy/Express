73e4de57e1494988150ef943c279de9a4ef10c7e
Susana Muñoz <su.munozh@gmail.com>
2023-07-13 21:04:58 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,35 @@
+//https://codeforgeek.com/render-html-file-expressjs/
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+const app = express();
+const json = require("./datos.json");
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1 style='color:red'>Hola Mundo!!!!</h1>");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.status(201).send("<h1 style='color:red'>Método POST</h1>");
+});
+
+app.put("/", (req, res) => {
+  // console.log(req);
+  res.send("<h1 style='color:red'>Método PUT</h1>");
+});
+
+app.get("/Contacto", (req, res) => {
+  res.json(json);
+});
+
+app.get("/sendFile", (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'))
+});
+
+
+
+app.listen(3000, () => {
+  console.log("Levantado puerto 3000");
+});
